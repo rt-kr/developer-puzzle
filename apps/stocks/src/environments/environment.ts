d59b8152da3791bbd796b99885bddbf8b6277622
Kumari <arti.a.kumari@accenture.com>
2020-02-06 21:17:07 +0530

    Task 1 Fixes

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,8 +6,8 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
-  apiURL: 'https://sandbox.iexapis.com'
+  apiKey: 'pk_64580a65fbc14f8598611aae1e137af9',
+  apiURL: 'https://cloud.iexapis.com'
 };
 
 /*
